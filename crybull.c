0fa264adf248238efe19214db23d49943b8b0ce2
Labolida <labolida.leonardo@gmail.com>
2024-06-14 15:13:40 +0200

    Update crybull.c

--- crybull.c
+++ crybull.c
@@ -11,18 +11,18 @@ int main( int argc , char ** argv ) {
 		printf("	crybull MySecretPassword  secretMessage.txt  encryptedMessage.txt	\n" ) ;
 		printf("	crybull MySecretPassword  encryptedMessage.txt  secretMessage.txt	\n\n" ) ;
 	}else{
-		char * pwd	  	= argv[1];		// STDIN.PARAM: PASSWORD
+		char * pwd	= argv[1];		// STDIN.PARAM: PASSWORD
 		char * fn_in	= argv[2];		// STDIN.PARAM: INPUT-FILENAME
 		char * fn_out   = argv[3];		// STDIN.PARAM: OUTPUT-FILENAME
-		FILE * fp1 ;					// FILE POINTER IN
-		FILE * fp2 ;					// FILE POINTER OUT
-		int s=0;						// step (position in while)
-		int c;							// char readed from file
+		FILE * fp1 ;				// FILE POINTER IN
+		FILE * fp2 ;				// FILE POINTER OUT
+		int s=0;				// step (position in while)
+		int c;					// char readed from file
 		fp1 = fopen( fn_in  , "r");
 		fp2 = fopen( fn_out , "w");
 		while( (c = fgetc(fp1))!=EOF ) {
 			int p = pwd[ ( s % strlen(pwd) ) ];
-            fputc( c^p  , fp2 );                    // CLASSIC XOR VIGENERE
+			fputc( c^p  , fp2 );                    // CLASSIC XOR VIGENERE
 			s++;
 		}
 		fclose(fp1);
